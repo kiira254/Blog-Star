e964daf8a2b22640a011258e631de6c111c69587
kiira254 <nkamotho57@gmail.com>
2020-09-19 04:34:28 +0300

    add configurations

--- app/main/views.py
+++ app/main/views.py
@@ -9,8 +9,8 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
-    
-@app.route('/pitch/<pitch_id>')
+
+@app.route('/pitch/<int:pitch_id>')
 def pitch(pitch_id):
 
     '''

2c7b8159a7ff0d95995da6fe7669618196ea0218
kiira254 <nkamotho57@gmail.com>
2020-10-01 00:23:36 +0300

    add changes

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ export SECRET_KEY='Flask WTF Secret Key'
 export SECRET_KEY="powerful secretkey"
 # export WTF_CSRF_SECRET_KEY="a csrf secret key"
 export MAIL_USERNAME='nkamotho69@gmail.com'
-export MAIL_PASSWORD='neshlike'
+export MAIL_PASSWORD='Nelly@'
 export FLASK_ENV="development"
 
 python3 manage.py server
